accaaf52e1da326f3521ab52f8e2b3b3fcc00df0
sebastiaan geleedst <beerbootking@gmail.com>
2021-04-26 19:11:50 +0200

    !!!!!!EPILEPSIE WAARSCHUWING!!!! niet al te veel klikken op 1234, Verder dan dat user input toegevoegd, kleur achtergrond veranderd zodra je op de 1 2 3 of 4 klikt.

--- desktop/src/com/mygdx/game/desktop/Kbsgame.java
+++ desktop/src/com/mygdx/game/desktop/Kbsgame.java
@@ -2,9 +2,14 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.awt.*;
@@ -16,6 +21,14 @@ public class Kbsgame extends ApplicationAdapter {
     private OrthographicCamera camera;
     private Texture bullet;
     private Rectangle heroBody;
+    //ShapeRenderer shapeRenderer;
+
+
+
+    float r = MathUtils.random();
+    float g = MathUtils.random();
+    float b = MathUtils.random();
+
     @Override
 
     public void create(){
@@ -38,10 +51,18 @@ public class Kbsgame extends ApplicationAdapter {
 
         //enemy
 
+       // shapeRenderer = new ShapeRenderer();
+
 
 
 
     }
+
+
+
+
+
+
 
     @Override
     public void render(){
@@ -55,5 +76,46 @@ public class Kbsgame extends ApplicationAdapter {
         batch.draw(heroImage, heroBody.x, heroBody.y);
         batch.end();
 
+        Gdx.gl.glClearColor(r, g, .25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)){
+            r = MathUtils.random();
+            g = MathUtils.random();
+            b = MathUtils.random();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)){
+            r = MathUtils.random();
+            g = MathUtils.random();
+            b = MathUtils.random();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3)){
+            r = MathUtils.random();
+            g = MathUtils.random();
+            b = MathUtils.random();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_4)){
+            r = MathUtils.random();
+            g = MathUtils.random();
+            b = MathUtils.random();
+        }
+
+
+
+
+
+
     }
-}
+
+
+        //
+
+    /*public void dispose () {
+        shapeRenderer.dispose();
+    }
+*/
+    }
+
